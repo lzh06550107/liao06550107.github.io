973ca22193ac7e8c160629f8914d1791a6605b63
lzh <liao08022040@gmail.com>
2016-06-14 09:19:23 +0000

    test

--- static/custom/js/qq_login.js
+++ static/custom/js/qq_login.js
@@ -1,7 +1,44 @@
+function callback(user) 
+{
+  var userName = document.getElementById('userName');
+  var greetingText = document.createTextNode('Greetings, '+ user.openid + '.');
+  userName.appendChild(greetingText);
+}
+
+function test(){
+	//应用的APPID，请改为你自己的
+	var appID = "101322571";
+	//成功授权后的回调地址，请改为你自己的
+	var redirectURI = "http://lzh06550107.github.io/";
+
+	//构造请求
+	if (window.location.hash.length == 0) 
+	{
+	  var path = 'https://graph.qq.com/oauth2.0/authorize?';
+	  var queryParams = ['client_id=' + appID,'redirect_uri=' + redirectURI,'scope=' + 'get_user_info,list_album,upload_pic,add_feeds,do_like','response_type=token'];
+
+	  var query = queryParams.join('&');
+	  var url = path + query;
+	  window.open(url,"TencentLogin","width=450,height=320,menubar=0,scrollbars=1,resizable=1,status=1,titlebar=0,toolbar=0,location=1");
+	}
+	else 
+	{
+	  //获取access token
+	  var accessToken = window.location.hash.substring(1);
+	  //使用Access Token来获取用户的OpenID
+	  var path = "https://graph.qq.com/oauth2.0/me?";
+	  //使用jsonp跨域访问方式
+	  var queryParams = [accessToken, 'callback=callback'];
+	  var query = queryParams.join('&');
+	  var url = path + query;
+	  var script = document.createElement('script');
+	  script.src = url;
+	  document.body.appendChild(script);     
+	}
+}
+
 $(document).ready(function(){
 	$("#qq-login").click(function(){
-   //以下为按钮点击事件的逻辑。注意这里要重新打开窗口
-   //否则后面跳转到QQ登录，授权页面时会直接缩小当前浏览器的窗口，而不是打开新窗口
-   var A=window.open("oauth/index.php","TencentLogin","width=450,height=320,menubar=0,scrollbars=1,resizable=1,status=1,titlebar=0,toolbar=0,location=1");
-};
+	test();
+});
 }); 
