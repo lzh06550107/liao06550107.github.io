8ecdd778b964884aef8afcf5f0d073bcf615a7d3
lzh <liao08022040@gmail.com>
2016-06-14 09:23:26 +0000

    third commit

--- static/custom/js/qq_login.js
+++ static/custom/js/qq_login.js
@@ -19,7 +19,7 @@ function test(){
 
 	  var query = queryParams.join('&');
 	  var url = path + query;
-	  window.open(url,"TencentLogin","width=450,height=320,menubar=0,scrollbars=1,resizable=1,status=1,titlebar=0,toolbar=0,location=1");
+	  window.open(url);
 	}
 	else 
 	{
